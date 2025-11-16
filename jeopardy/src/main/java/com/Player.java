260c2dfca545517338ca57815d61ee28e4acbe95
lifexpert0-beep <lifexpert0@gmail.com>
2025-11-15 22:10:59 -0400

    Player Class Setup

--- jeopardy/src/main/java/com/Player.java
+++ jeopardy/src/main/java/com/Player.java
@@ -9,7 +9,7 @@ public class Player {
 
     public Player(String name){
 
-        this.id = UUID.randomUUID().toString();;
+        this.id = UUID.randomUUID().toString();
         this.name = name;
         this.score = 0;
     }
