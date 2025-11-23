3708f9ef7c9df7eb6657252d2322385503d89c75
lifexpert0-beep <lifexpert0@gmail.com>
2025-11-23 16:23:44 -0400

    Base SelectQuestion Logic w/o Error Checking

--- jeopardy/src/main/java/com/project/Main.java
+++ jeopardy/src/main/java/com/project/Main.java
@@ -1,8 +1,5 @@
 package com.project;
 
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         
@@ -14,40 +11,18 @@ public class Main {
         GameEngineCommand cmd1 = new SelectPlayerCountCommand(xp);
         GameEngineCommand cmd2 = new EnterPlayerNameCommand(xp);
         GameEngineCommand cmd3 = new SelectCategoryCommand(xp);
+        GameEngineCommand cmd4 = new SelectQuestionCommand(xp);
         
         system.runCommand(cmd1);
         system.runCommand(cmd2);
         system.runCommand(cmd3);
+        system.runCommand(cmd4);
 
         //LoadFileCommand cmd = new LoadFileCommand(manager, "src/main/resources/sample_game_JSON.json");
         //LoadFileCommand cmd = new LoadFileCommand(manager, "src/main/resources/sample_game_XML.xml");
         cmd.execute();
 
         System.out.println("Successfully loaded " + manager.getQuestions().size() + " questions\n");
-
-        
-        //TEST FOR CATERGORY MANAGER
-        var parser = new CSVParserAdaptee();  
-        //DIFFERENT PARSER OPTIONS
-        //var JsonParser = new JSONParserAdaptee();
-        //var XMLParser = new XMLParserAdaptee();               
-        List<Question> questions = parser.parse("jeopardy/src/main/resources/sample_game_CSV.csv");
-        //List<Question> questions = parser.parse("jeopardy/src/main/resources/sample_game_JSON.json");
-        //List<Question> questions = parser.parse("jeopardy/src/main/resources/sample_game_XML.xml");
-        var category = new CategoryManager();
-        questions.forEach(category::addQuestion);
-
-        System.out.println("Categories: " + category.size());
-        for (String cat : category.getCategoryNames()) {
-            System.out.println(" ");
-            System.out.println(cat);
-            List<Question> list = category.getQuestions(cat);
-            Collections.sort(list, (a,b) -> a.getValue() - b.getValue());
-            for (Question q : list) {
-            System.out.println("$" + q.getValue() + " " + q.getQuestion());
-            }
-        }
-        
     }
 }   
     
