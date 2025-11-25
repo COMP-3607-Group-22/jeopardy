70bcfdc0a2cf880c66b23c7766efe6da2589e58e
lifexpert0-beep <lifexpert0@gmail.com>
2025-11-25 18:24:35 -0400

    Design Fixes and Category Exhausted Function Added

--- jeopardy/src/main/java/com/project/Gameplay/GameEngine.java
+++ jeopardy/src/main/java/com/project/Gameplay/GameEngine.java
@@ -18,6 +18,7 @@ public class GameEngine {
     private Player lastPlayer;
     private int currentPlayerIndex = 0;
     private int totalTurns = 1;
+    private boolean categoryEmpty;
 
     CategoryManager category;
     ArrayList<Player> players;
@@ -37,13 +38,14 @@ public class GameEngine {
         this.currentCategory = null;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Select a category from the ones below");
+        System.out.println("\nSelect a category from the ones below\n");
+        System.out.println("-------------------------------------------------");
 
         for (String cat : category.getCategoryNames()) {
-            System.out.println(" ");
             System.out.println(cat);
         }
-        System.out.println("\n");
+        System.out.println("-------------------------------------------------");
+        System.out.print("\nYour requested category : ");
         String chosenCategory = scanner.nextLine();
 
         for (String cat : category.getCategoryNames()) {
@@ -53,51 +55,57 @@ public class GameEngine {
             }
         }
         if (found == false){
-            System.out.println("Invalid Category.");
+            System.out.println("\nInvalid Category.");
             selectCategory();
         }
         else{
+            System.out.println("\n*****************************************");
             System.out.println("You have choosen " + this.currentCategory);
+            System.out.println("*****************************************");
         }
     }
 
     public void selectQuestion(){
         boolean found = false;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Select a question by inputting the dollar value (The $ is there already)");
+        System.out.println("\nSelect a question by inputting the dollar value.\n");
         List<Question> list = category.getQuestions(this.currentCategory);
         Collections.sort(list, (a,b) -> a.getValue() - b.getValue());
         for (Question q : list) {
             System.out.println("$" + q.getValue());
             }
+        System.out.print("\nYour requested question : $");    
         int chosenQuestion = scanner.nextInt();
         for (Question q : list) {
             if(chosenQuestion == q.getValue()){
                 this.currentQuestion = q;
-                System.out.println("You have choosen the question for $" + chosenQuestion);
+                System.out.println("\n*****************************************");
+                System.out.println("\nYou have choosen the question for $" + chosenQuestion);
+                System.out.println("\n*****************************************");
                 found=true;
             }
         }
         if(found == false){
-            System.out.println("Invalid Question Choice.\n");
+            System.out.println("\nInvalid Question Choice.\n");
             selectQuestion();
         }
 
-        System.out.println("For $" + currentQuestion.getValue() + "\n" + currentQuestion.getQuestion() + "\n" + currentQuestion.getOptions());
+        System.out.println("\nFor $" + currentQuestion.getValue() + "\n\n" + currentQuestion.getQuestion() + "\n\n" + currentQuestion.getOptions());
     }
 
     public void answerQuestion(){
         Scanner scanner = new Scanner(System.in);
+        System.out.print("\nYour Choice : ");
         this.givenAnswer = scanner.nextLine();
 
         if(isAnswerCorrect()){
-            System.out.println("You got the answer correct! yay");
+            System.out.println("\nYou got the answer correct!");
             currentPlayer.setScore(currentQuestion.getValue());
-            System.out.println("Your new score is " + currentPlayer.getScore());
+            System.out.println("\nYour new score is " + currentPlayer.getScore());
         }
         else{
-            System.out.println("The correct answer was: " + currentQuestion.getAnswer());
-            System.out.println("Your new score is " + currentPlayer.getScore());
+            System.out.println("\nThe correct answer was: " + currentQuestion.getAnswer());
+            System.out.println("\nYour new score is " + currentPlayer.getScore());
         }
 
         category.removeQuestion(this.currentCategory, this.currentQuestion); //removes questions from list
@@ -105,6 +113,15 @@ public class GameEngine {
         List<Question> remainingQuestions = category.getQuestions(this.currentCategory); //remove empty categories from list
         if (remainingQuestions.isEmpty()) {
             category.removeCategory(this.currentCategory);
+        }
+
+        if(category.getAllCatergories().isEmpty()){
+            System.out.println("\n*****************************************");
+            System.out.println("No more categories exist. The game has included");
+            System.out.println("\n*****************************************");
+            this.categoryEmpty = true;
+        } else{
+             this.categoryEmpty = false;
         }
 
         addTurnSummary();
@@ -116,11 +133,17 @@ public class GameEngine {
         this.lastPlayer = this.currentPlayer;
         this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.size();
         this.currentPlayer = this.players.get(this.currentPlayerIndex);
-        System.out.println("\nIt is now " + this.currentPlayer.getName() + "'s turn!\n");
+        System.out.println("\n---------------------------------");   
+        System.out.println("It is now " + this.currentPlayer.getName() + "'s turn!");
+        System.out.println("---------------------------------");
     }
 
     public String getCurrentCategory() {
         return this.currentCategory;
+    }
+
+     public boolean getCategoryEmpty() {
+        return this.categoryEmpty;
     }
 
     public Question getCurrentQuestion() {
